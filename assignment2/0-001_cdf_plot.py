51cf10caaaca5ae347878936e4e566f6efef991b
Live session user <alexander.higson@mansfield.ox.ac.uk>
2025-06-09 16:54:01 +0100

    Exercise2 09/06/25

---
+++ assignment2/0-001_cdf_plot.py
@@ -0,0 +1,25 @@
+# !/usr/bin/python3
+import numpy as np
+import matplotlib.pyplot as plt
+
+# parameters to modify
+filename="processed_ping0-001.txt"
+label='label'
+xlabel = 'time'
+ylabel = 'cdf'
+title='CDF plot'
+fig_name='ping0-001.png'
+bins=1000 #adjust the number of bins to your plot
+
+
+t = np.loadtxt(filename, delimiter=" ", dtype="float")
+
+#plt.plot(t[:,0], t[:,1], label=label)  # Plot some data on the (implicit) axes.
+#Comment the line above and uncomment the line below to plot a CDF
+plt.hist(t[:], bins, density=True, histtype='step', cumulative=True, label=label)
+plt.xlabel(xlabel)
+plt.ylabel(ylabel)
+plt.title(title)
+plt.legend()
+plt.savefig(fig_name)
+plt.show()
